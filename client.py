02a85e821e4c03e73a92194209cae7aba666eaf2
Kimaru Thagana <30570908+KimaruThagna@users.noreply.github.com>
2018-02-06 10:46:48 +0000

    Update client.py

--- client.py
+++ client.py
@@ -5,3 +5,4 @@ def rpc_call(command):
 	data_received = proxy.root.line_counter(command)
 	return data_received
 print(rpc_call("pwd")) # this will return the current working directory.
+# at this point, you can place any linux command other than pwd
